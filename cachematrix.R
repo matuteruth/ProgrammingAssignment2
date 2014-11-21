267cb90874a9cd1b79231ebda64097c08b17225f
matuteruth <matuteruth@gmail.com>
2014-11-21 16:07:25 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,48 @@
 ## functions do
 
 ## Write a short comment describing this function
+makecacheMatrix <- function(x = numeric()) {      # input x will be a vector
 
-makeCacheMatrix <- function(x = matrix()) {
+m <- NULL    #  m will be our 'mean' and it's reset to NULL every 
+#    time makeVector is called
 
+#  note these next three functions are defined but not run when makeVector is called.
+#   instead, they will be used by cachemean() to get values for x or for
+#   m (mean) and for setting the mean.  These are usually called object 'methods'
+
+get <- function() {
+  x 
+}   # this function returns the value of the original vector
+
+setmean <- function(mean)  {
+  m <<- mean
 }
+# this is called by cachemean() during the first cachemean()
+#  access and it will store the value using superassignment
+
+getmean <- function() { m } # this will return the cached value to cachemean() on
+#  subsequent accesses
+
+list(get = get,          #  OK, this is accessed each time makeVector() is called,       
+     setmean = setmean,  #   that is, each time we make a new object.  This is a list of 
+     getmean = getmean)  #   the internal functions ('methods') so a calling function
+#   knows how to access those methods.                            
+}
+
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {   # the input x is an object created by makeVector
+  m <- x$getSolve()               # accesses the object 'x' and gets the value of the mean
+  if(!is.null(m)) {              # if mean was already cached (not NULL) ...
+    
+    message("getting cached data")  
+    return(m)                       # ... and return the mean ... "retu
+  }
+  data <- x$get()       
+  m <- Solve(data, ...)   # if m was NULL then we have to calculate the mean
+  x$setmean(m)           # store the calculated mean value in x (see setmean() in makeVector
+  m               # return the mean to the code that called this function
 }
+
